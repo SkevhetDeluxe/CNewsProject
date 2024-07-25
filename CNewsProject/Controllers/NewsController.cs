3a1a249deca9bb7d6f5747d830150c1698b19fa2
iskoglund <iskoglund@yahoo.com>
2024-07-25 15:55:47 +0200

    I updated NewsController.

--- CNewsProject/Controllers/NewsController.cs
+++ CNewsProject/Controllers/NewsController.cs
@@ -2,27 +2,37 @@
 
 namespace CNewsProject.Controllers
 {
+    [Route("news")]
     public class NewsController : Controller
-    {
+    {        
+        [Route("local")]
         public IActionResult Local()
         {
             return View();
         }
+
+        [Route("sweden")]
 
         public IActionResult Sweden()
         {
             return View();
         }
 
+        [Route("world")]
+
         public IActionResult World()
         {
             return View();
         }
 
+        [Route("economy")]
+
         public IActionResult Economy()
         {
             return View();
         }
+
+        [Route("sport")]
 
         public IActionResult Sport()
         {
