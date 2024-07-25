7dc2292d8fe4ed53aee4fd72c61de3e1bcd761a8
iskoglund <iskoglund@yahoo.com>
2024-07-25 14:07:14 +0200

    I added views to different categories of news (local, Swedish, etc). I added NewsController that rules the buttons in the navbar.

---
+++ CNewsProject/Controllers/NewsController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CNewsProject.Controllers
+{
+    public class NewsController : Controller
+    {
+        public IActionResult Local()
+        {
+            return View();
+        }
+
+        public IActionResult Sweden()
+        {
+            return View();
+        }
+
+        public IActionResult World()
+        {
+            return View();
+        }
+
+        public IActionResult Economy()
+        {
+            return View();
+        }
+
+        public IActionResult Sport()
+        {
+            return View();
+        }
+    }
+}
