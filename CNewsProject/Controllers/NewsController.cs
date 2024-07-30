30b4ba91513563b9f32f054779f5fdeafda1dd35
Biku <victoria.astrom@gmail.com>
2024-07-30 10:29:46 +0200

    undid some changes from yesterday that broke the site

--- CNewsProject/Controllers/NewsController.cs
+++ CNewsProject/Controllers/NewsController.cs
@@ -1,51 +1,55 @@
 ﻿using CNewsProject.Models.DataBase;
-using CNewsProject.Services;
+using CNewsProject.Service;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CNewsProject.Controllers
 {
-	[Route("news")]
 	public class NewsController : Controller
 	{
-		private readonly IArticleService _articleService;
+		// DONT KNOW WHY THIS DOESNT WORK TBH
+		// PLEASE LET ME KNOW
 
-		public NewsController(IArticleService articleService)
+		//private readonly IArticleService _articleService;
+		//private readonly ICategoryService _categoryService;
+
+		public NewsController(/*IArticleService articleService, ICategoryService categoryService*/)
 		{
-			_articleService = articleService;
+			//_articleService = articleService;
+			//_categoryService = categoryService;
 		}
 		public IActionResult Local()
 		{
-			Category category = new();
-			List<Article> localArticles = _articleService.GetArticleListByCategory(category);
-			return View(localArticles);
+			//Category category = _categoryService.GetCategoryByName("Local"); // Maybe change this to ID later when we have database ready
+			//List<Article> localArticles = _articleService.GetArticleListByCategory(category);
+			return View(/*localArticles*/);
 		}
 
 		public IActionResult Sweden()
 		{
-			Category category = new();
-			List<Article> swedenArticles = _articleService.GetArticleListByCategory(category);
-			return View(swedenArticles);
+			//Category category = _categoryService.GetCategoryByName("Sweden"); // Maybe change this to ID later when we have database ready
+			//List<Article> swedenArticles = _articleService.GetArticleListByCategory(category);
+			return View(/*swedenArticles*/);
 		}
 
 		public IActionResult World()
 		{
-			Category category = new();
-			List<Article> worldArticles = _articleService.GetArticleListByCategory(category);
-			return View(worldArticles);
+			//Category category = _categoryService.GetCategoryByName("World"); // Maybe change this to ID later when we have database ready
+			//List<Article> worldArticles = _articleService.GetArticleListByCategory(category);
+			return View(/*worldArticles*/);
 		}
 
 		public IActionResult Economy()
 		{
-			Category category = new();
-			List<Article> economyArticles = _articleService.GetArticleListByCategory(category);
-			return View(economyArticles);
+			//Category category = _categoryService.GetCategoryByName("Economy"); // Maybe change this to ID later when we have database ready
+			//List<Article> economyArticles = _articleService.GetArticleListByCategory(category);
+			return View(/*economyArticles*/);
 		}
 
 		public IActionResult Sport()
 		{
-			Category category = new();
-			List<Article> sportArticles = _articleService.GetArticleListByCategory(category);
-			return View(sportArticles);
+			//Category category = _categoryService.GetCategoryByName("Sport"); // Maybe change this to ID later when we have database ready
+			//List<Article> sportArticles = _articleService.GetArticleListByCategory(category);
+			return View(/*sportArticles*/);
 		}
 	}
 }
