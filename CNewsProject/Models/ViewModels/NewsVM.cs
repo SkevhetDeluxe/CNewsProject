b85f2be0028bd4de005a2d588fecf17e7ee0b6ac
iskoglund <iskoglund@yahoo.com>
2024-08-01 14:05:54 +0200

    I corrected errors

---
+++ CNewsProject/Models/ViewModels/NewsVM.cs
@@ -0,0 +1,13 @@
+﻿using CNewsProject.Models.Api.ExchangeRates;
+using CNewsProject.Models.Api.Weather;
+using CNewsProject.Models.DataBase;
+
+namespace CNewsProject.Models.ViewModels
+{
+    public class NewsVM
+    {
+        public WeatherStats WeatherStats { get; set; }
+        public ExchangeRates ExchangeRates { get; set; }
+        public List<Article> Articles { get; set; }
+    }
+}
