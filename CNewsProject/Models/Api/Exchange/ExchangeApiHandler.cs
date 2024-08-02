c1984a17ed6b8182ffdc830c38d617dfb0758d15
Joakim-B987 <joakim.bjornskar@hotmail.com>
2024-08-02 09:39:22 +0200

    Part  1 done

---
+++ CNewsProject/Models/Api/Exchange/ExchangeApiHandler.cs
@@ -0,0 +1,47 @@
+﻿using Azure.Core;
+using Azure;
+using System.Net.Http.Headers;
+using System.Text.Json;
+
+namespace CNewsProject.Models.Api.Exchange
+{
+    public class ExchangeApiHandler : IExchangeApiHandler
+    {
+        private readonly HttpClient _client;
+
+        public ExchangeApiHandler(HttpClient client)
+        {
+            _client = client;
+        }
+
+        public async Task<Exchange> GetExchangeAsync(string country)
+        {
+            var request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri("https://real-time-finance-data.p.rapidapi.com/stock-time-series-source-2?symbol=AAPL&period=1D"),
+                Headers =
+                {
+                    { "x-rapidapi-key", "788a749ec0mshbc966b8c428eee0p1fd99djsn15f6ea339f04" },
+                    { "x-rapidapi-host", "real-time-finance-data.p.rapidapi.com" },
+                },
+            };
+
+            try
+            {
+                using (var response = await _client.SendAsync(request))
+                {
+                    response.EnsureSuccessStatusCode();
+                    var body = await response.Content.ReadAsStringAsync();
+                    var exchange = JsonSerializer.Deserialize<Exchange>(body);
+                    return exchange;
+                }
+            }
+            catch (HttpRequestException e)
+            {
+                Console.WriteLine($"Request error: {e.Message}");
+                return null;
+            }
+        }
+    }
+}
