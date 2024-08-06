a08375a745f5212ebd8c081d45ce3f9bd4906529
bikuastrom <Victoria.astrom@gmail.com>
2024-08-06 16:00:44 +0200

    added a second layout file so we can have weather/exchange on the news pages and dont have to have it on every single news view

--- CNewsProject/Service/IArticleService.cs
+++ CNewsProject/Service/IArticleService.cs
@@ -4,49 +4,49 @@ using CNewsProject.Models.ViewModels;
 
 namespace CNewsProject.Service
 {
-    public interface IArticleService
-    {
-        public string UploadBlob(IFormFile articleImage, string fileName);
+	public interface IArticleService
+	{
+		
+		public Article GetArticleById(int Id);
+		public List<Article> GetAllArticles();
+		public List<Article> GetAllPublished();
+		public void EditArticle(Article article);
+		public List<Article> GetPendingArticles();
+		public void RemoveArticle(Article article);
+		public void UpdateArticle(Article article);
+		public void DeclineArticle(int id, string reason);
+		public FrontPageArticlesVM GetFrontPageArticleVM();
+		public void PublishArticle(int id, string publisherName);
+		public string UploadBlob(IFormFile articleImage, string fileName);
+		public void WriteArticle(WriteArticleVM newArticle, string authorName);
+		public List<Article> SearchForArticles(string search, string category);
+		public List<Article> GetArticleListByCategoryStringified(string category);
+		
 
-        public List<Article> GetAllArticles();
-        public List<Article> GetAllPublished();
-        public Article GetArticleById(int Id);
-        public FrontPageArticlesVM GetFrontPageArticleVM();
-        public List<Article> GetPendingArticles();
-        public void WriteArticle(WriteArticleVM newArticle, string authorName);
-        public void RemoveArticle(Article article);
-        public void EditArticle(Article article);
-        public void UpdateArticle(Article article);
-        public void PublishArticle(int id, string publisherName);
-        public List<Article> SearchForArticles(string search, string category);
-        public List<Article> GetArticleListByCategoryStringified(string category);
-        public void DeclineArticle(int id, string reason)
-;
-
-        //public List<SelectListItem> GetHeadlineList();
-        //public List<SelectListItem> GetCategoryList();
-
-
-        #region UpdateVM()_Overloads.
-        //public ArticleListVM UpdateVM();
-        //public ArticleListVM UpdateVM(ArticleListVM vModel);
-        //public ArticleListVM UpdateVM(int id, int pageSize);
-        //public ArticleListVM UpdateVM(int id, int pageSize, ArticleListVM vModel);
-        #endregion
+		//public List<SelectListItem> GetHeadlineList();
+		//public List<SelectListItem> GetCategoryList();
 
 
-        #region Only_For_FrontPage
+		#region UpdateVM()_Overloads.
+		//public ArticleListVM UpdateVM();
+		//public ArticleListVM UpdateVM(ArticleListVM vModel);
+		//public ArticleListVM UpdateVM(int id, int pageSize);
+		//public ArticleListVM UpdateVM(int id, int pageSize, ArticleListVM vModel);
+		#endregion
 
-        //// Not sure if access is needed to these.
-        //// Leaving them accessable for now.
-        ////public List<Article> FPMostPopular();
-        //public List<Article> FPNewest();
-        //public List<Article> FPOldest();
-        ////public List<Article> FPCheapest();
 
-        ////public FrontPageVM SetupFPVM();
+		#region Only_For_FrontPage
 
-        #endregion
-    }
+		//// Not sure if access is needed to these.
+		//// Leaving them accessable for now.
+		////public List<Article> FPMostPopular();
+		//public List<Article> FPNewest();
+		//public List<Article> FPOldest();
+		////public List<Article> FPCheapest();
+
+		////public FrontPageVM SetupFPVM();
+
+		#endregion
+	}
 }
 
