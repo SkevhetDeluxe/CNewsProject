abe98ccdd6a4126f289a1331f2d93ba6362f907f
Shams Nasiri <nasirishams50@gmail.com>
2024-08-12 10:25:35 +0200

    Added Lock Articles newer than 48 h

---
+++ CNewsProject/Service/ArticleLockService.cs
@@ -0,0 +1,15 @@
+﻿using CNewsProject.Models.DataBase;
+using System;
+namespace CNewsProject.Service
+{
+    public class ArticleLockService : IArticleLockService
+    {
+        public bool IsArticleLocked(Article article)
+        {
+            TimeSpan timeSincePublished = DateTime.Now - article.PublishedDate;
+
+            
+            return timeSincePublished.TotalHours > 48;
+        }
+    }
+}
