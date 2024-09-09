6b81c20bd8d350a125813f799bca598ef8166bfe
iskoglund <iskoglund@yahoo.com>
2024-09-09 09:10:00 +0200

    WeatherArchive

--- CNewsFunctions/WeatherArchive.cs
+++ CNewsFunctions/WeatherArchive.cs
@@ -10,18 +10,18 @@ namespace CNewsFunctions
 {
     public class WeatherArchive
     {
-        private readonly ILogger _logger;
+        private readonly ILogger log;
 
         public WeatherArchive(ILoggerFactory loggerFactory)
         {
-            _logger = loggerFactory.CreateLogger<WeatherArchive>();
+            log = loggerFactory.CreateLogger<WeatherArchive>();
         }
 
         [Function("WeatherArchive")]
         public async Task Run([TimerTrigger("0 0 3 * * *")] TimerInfo myTimer)
         {
             // Log the start of the function execution
-            _logger.LogInformation($"C# Timer trigger function executed at: {DateTime.Now}");
+            log.LogInformation($"C# Timer trigger function executed at: {DateTime.Now}");
 
             string apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=Stockholm&appid=YOUR_API_KEY";
             using HttpClient httpClient = new HttpClient();
@@ -36,7 +36,7 @@ namespace CNewsFunctions
             string condition = jsonObject["weather"][0]["description"].Value<string>();
 
             // Log the extracted data
-            _logger.LogInformation($"Fetched weather data: Temperature - {temperature}, Condition - {condition}");
+            log.LogInformation($"Fetched weather data: Temperature - {temperature}, Condition - {condition}");
 
             // Store the weather data in Azure Table Storage
             var connString = "DefaultEndpointsProtocol=https;AccountName=cnewsstorage;AccountKey=42s4C494d16TS+Ww3wwbWFcx3Nn2SuAsL6aJTsbfLnZqoND5gJ6O69MGdzSX69h6YrQTMgyBJ0t++AStJI5xcA==;EndpointSuffix=core.windows.net";
@@ -51,7 +51,7 @@ namespace CNewsFunctions
             // Insert the entity into Azure Table Storage
             await tableClient.AddEntityAsync(weatherEntity);
 
-            _logger.LogInformation($"Weather data archived successfully at: {DateTime.UtcNow}");
+            log.LogInformation($"Weather data archived successfully at: {DateTime.UtcNow}");
 
         }
     }
