66080074a3e996dc77ad54023d0b61ba2d306c0b
SkevhetDeluxe <filip.bergfjord@protonmail.com>
2024-09-09 08:58:27 +0200

    Shit Commit - Resize, Edit ARticle update.

--- CNewsFunctions/ArchiveNews.cs
+++ CNewsFunctions/ArchiveNews.cs
@@ -1,18 +1,19 @@
 using System;
 using CNewsProject.Data;
 using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
 namespace CNewsFunctions;
 
-public class ArchiveNews(ILogger<ArchiveNews> logger, FunctionDbContext db)
+public class ArchiveNews(ILogger<ArchiveNews> logger, FunctionDbContext db, IConfiguration config)
 {
     [Function("ArchiveNews")] // Assign False to RunOnStartup Parameter before PUBLISHING
-    public void Run([TimerTrigger("0 0 * * 5", RunOnStartup = true)] TimerInfo myTimer)
+    public void Run([TimerTrigger("0 0 * * * * ", RunOnStartup = false)] TimerInfo myTimer)
     {
         logger.LogInformation($"C# Timer trigger function executed at: {DateTime.Now}");
-
-        var articles = db.Article.ToList();
+        
+        var articles = db.Article.Where(a => a.Status == "Approved").ToList();
         var toArchive = articles.Where(a => a.PublishedDate <= DateTime.Now.AddDays(-30)).ToList();
         //var toArchiveIds = toArchive.Select(a => a.Id).ToList();
 
@@ -31,3 +32,11 @@ public class ArchiveNews(ILogger<ArchiveNews> logger, FunctionDbContext db)
         }
     }
 }
+
+// STATUSES FOR ARTICLES
+/*
+    Approved = Saved in DB. Users can READ
+    Draft = Saved in DB. Not readable by anyone else but Author.
+    Declined = Saved in DB. Only readable by author and needs to be resubmitted.
+
+*/
