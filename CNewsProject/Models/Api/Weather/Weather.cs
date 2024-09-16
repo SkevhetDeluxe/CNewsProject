26bd29ac6d7c6dd1250399f41ce96d845bafa35b
Joakim-B987 <joakim.bjornskar@hotmail.com>
2024-09-16 11:04:12 +0200

    Part 1 done

--- CNewsProject/Models/Api/Weather/Weather.cs
+++ CNewsProject/Models/Api/Weather/Weather.cs
@@ -12,7 +12,7 @@ namespace CNewsProject.Models.Api.Weather
 		public int utc_offset_seconds { get; set; }
 		public string timezone { get; set; }
 		public string timezone_abbreviation { get; set; }
-		public int elevation { get; set; }
+		//public int elevation { get; set; }
 		public Current_Units current_units { get; set; }
 		public Current current { get; set; }
 		public Hourly_Units hourly_units { get; set; }
@@ -77,62 +77,3 @@ namespace CNewsProject.Models.Api.Weather
 	}
 
 }
-
-#region SMHI API
-
-//public class TestingPurpose
-//{
-//    public WeatherStats EmptyStats { get; set; } = new();
-//    public TestingPurpose()
-//    {
-
-//    }
-
-//    public void test()
-//    {
-//        //TempStat newTemp = EmptyStats.timeSeries[0].TempStats[0];
-//        //2024-08-07T11:00:00Z
-//        //{2024-08-07 17:00:00}
-
-//        DateTime testTime = new();
-//        testTime = Convert.ToDateTime("2024-08-07 17:00:00");
-
-//        var test = EmptyStats.timeSeries.FirstOrDefault(ts => ts.validTime == testTime).parameters.FirstOrDefault(p => p.name == "t");
-
-//        //var test = EmptyStats.timeSeries[0].TempStats.FirstOrDefault(s => s.name == "t");
-//    }
-//}
-
-public class WeatherStatsSMHI
-{
-	public DateTime approvedTime { get; set; }
-	public DateTime referenceTime { get; set; }
-	public Geometry geometry { get; set; }
-	public List<Timesery> timeSeries { get; set; }
-	public string NameOfCity { get; set; } = "Stockholm";
-}
-
-public class Geometry
-{
-	public string type { get; set; }
-	public float[][] coordinates { get; set; }
-}
-
-public class Timesery
-{
-	public DateTime validTime { get; set; }
-	public List<Parameter> parameters { get; set; }
-}
-
-public class Parameter
-{
-	public string name { get; set; }
-	public string levelType { get; set; }
-	public int level { get; set; }
-	public string unit { get; set; }
-	public float[] values { get; set; }
-}
-
-
-#endregion
-
