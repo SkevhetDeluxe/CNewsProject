957ffbd442c64fd5c8ba2579a5d4ae47cedfc49f
iskoglund <iskoglund@yahoo.com>
2024-09-19 15:32:39 +0200

    I added exchange rate history view.

---
+++ CNewsProject/Models/ViewModels/ExchangeRateHistoryViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace CNewsProject.Models.ViewModels
+{
+    public class ExchangeRateHistoryViewModel
+    {
+        public DateTime Date { get; set; }
+        public decimal USD { get; set; }
+        public decimal EUR { get; set; }
+        public decimal GBP { get; set; }
+    }
+}
